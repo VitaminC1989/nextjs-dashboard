1960b60d8f4848f01d111fc8156ddd92493db7a2
VitaminC1989 <1741253832@qq.com>
2023-12-10 15:31:40 +0800

    feat: 学习进度 Chapter 8 Static and Dynamic Rendering https://nextjs.org/learn/dashboard-app/static-and-dynamic-rendering

---
+++ app/dashboard/api/route.ts
@@ -0,0 +1,16 @@
+export async function GET() {
+  // const res = await fetch('https://data.mongodb-api.com/...', {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'API-Key': process.env.DATA_API_KEY,
+  //   },
+  // })
+  // const data = await res.json()
+
+  return Response.json({
+    data: {
+      code: 200,
+      msg: "操作成功",
+    },
+  });
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,50 @@
-import NavLinks from "@/app/ui/dashboard/nav-links";
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "@/app/lib/data";
 
-export default function Page() {
+export default async function Page() {
+  const revenuePromise = await fetchRevenue();
+  const latestInvoicesPromise = await fetchLatestInvoices();
+  const cardDataPromise = await fetchCardData();
+
+  const [revenue, latestInvoices, cardData] = await Promise.all([
+    revenuePromise,
+    latestInvoicesPromise,
+    cardDataPromise,
+  ]);
+
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = cardData;
+
   return (
-    <>
-      <p>Dashboard Page</p>
-      <NavLinks />
-    </>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
   );
 }
